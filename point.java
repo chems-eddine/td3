c6ab1dbb0a397869fdf92c5fb588d57d6e57ac4d
chems-eddine <chemsou.b@yahoo.com>
2016-10-30 08:44:01 +0000

    Update point.java

--- point.java
+++ point.java
@@ -1,31 +1,31 @@
 public class point {
 	private double abscisse;
-	private double ordonée;
+	private double ordonnée;
 	
 	public point(){
 		this.abscisse=0;
-		this.ordonée=0;}
+		this.ordonnée=0;}
 	public point(double x){
 		this.abscisse=x;
-		this.ordonée=x;
+		this.ordonnée=x;
 	}
 	public point (double x, double y){
 		this.abscisse=x;
-		this.ordonée=y;
+		this.ordonnée=y;
 	}
 	public double getAbscisse(){
 		return this.abscisse;
 	}
-	public double getOrdonée(){
-		return this.ordonée;
+	public double getOrdonnée(){
+		return this.ordonnée;
 	}
 	public void deplace ( int dx, int dy ){
 	
 	x+=dx;y+=dy;
 	}
 	public String toString(){
-		return "(" + abscisse+ ","+ ordonée +")";
+		return "(" + abscisse+ ","+ ordonnée +")";
 	}
 	public point Symetrie(){
-		return new point(-abscisse,ordonée);
+		return new point(-abscisse,ordonnée);
 	}
